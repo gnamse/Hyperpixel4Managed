1ac9d7bd7c66ea536eaec4a6f0f3fb22586918a9
Daniel.Onamusi <daniel.onamusi@hrsid.com>
2019-07-22 16:03:06 +0100

    Update Hyperpixel4.csproj, Program.cs, and 5 more files...

---
+++ src/Hyperpixel4/Program.cs
@@ -0,0 +1,15 @@
+﻿using HyperPixel4;
+using System;
+
+namespace Hyperpixel4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hyperpixel4Managed.Init();
+            Console.WriteLine("Started Hyperpixel4 Managed");
+            Console.ReadLine();
+        }
+    }
+}
